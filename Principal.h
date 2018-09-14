b75c84e95719faac0ee276030ffd395e5fcee7b7
Jesus <chjesussao@gmail.com>
2018-09-14 18:16:07 -0400

    opciones

--- Principal.h
+++ Principal.h
@@ -1,18 +1,28 @@
 #ifndef PRINCIPAL_H
 #define PRINCIPAL_H
 
+#include <stdlib.h>
 #include <conio.h>
+#include <vector>
+#include <time.h>
 
 using namespace std;
 class Principal{
+	vector<int> opcs;
+	int datoInit;
 	public:
 		void main();
+		void llenarVector(int datoInit);
+		void initJugador();
+		void jugar();
 };
 
 void Principal::main(){
 	bool band = true;
 	char tecla;
 	
+	srand(time(NULL));
+
 	do{
 		cout<<"1.- Ingresar dato inicial."<<endl;
 		cout<<"2.- Dato inicial Random."<<endl;
@@ -20,14 +30,27 @@ void Principal::main(){
 		tecla = getch();
 		switch(tecla){
 			case '1':
-				system("cls");
-				cout<<"Opcion 1."<<endl;
+				do{
+					system("cls");
+					cout<<"Ingrese un dato inicial entre 10 - 1000 : ";
+					cin>>datoInit;
+					if(datoInit<10 || datoInit>1000){
+						cout<<endl<<"El dato no esta en el rango requerido!"<<endl;
+						system("PAUSE");
+					}
+				}while(datoInit<10 || datoInit>1000);
+				Principal::llenarVector(datoInit);
+				Principal::jugar();
+				cout<<endl;
 				system("PAUSE");
 				system("cls");
 				break;
 			case '2':
 				system("cls");
-				cout<<"Opcion 1."<<endl;
+				datoInit = rand()%990+10;
+				Principal::llenarVector(datoInit);
+				Principal::jugar();
+				cout<<endl;
 				system("PAUSE");
 				system("cls");
 				break;
@@ -44,5 +67,21 @@ void Principal::main(){
 	}while(band);
 	
 	cout<<"Fin del Programa.";
+}
+
+void Principal::llenarVector(int datoInit){
+	int opciones = 1;
+	
+	while(opciones*opciones<=datoInit){
+		opcs.push_back(opciones);
+		opciones++;
+	}
+}
+
+void Principal::jugar(){
+	cout<<"Tablero: "<<endl;
+	for(int i=0;i<opcs.size();i++){
+		cout<<opcs[i]<<"^2  ";
+	}
 }
 #endif
